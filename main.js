1a74d8bea04e2b1cfe4ad5098add80ce5f3b437b
suxrob ondamurodov <ondamurodovsukhrob0716@gmail.com>
2023-09-19 17:30:45 +0500

    uygavazifa3

---
+++ main.js
@@ -0,0 +1,36 @@
+// let hk = prompt("hafta kuni kiriting");  
+// if(hk==1){
+//  console.log('du');
+// }else if(hk==2){
+//     console.log('se');
+// }else if(hk==3){
+//     console.log('cho');
+// }else if(hk==4){
+//     console.log('pa');
+// }else if(hk==5){
+//     console.log('ju');
+// }else if(hk==6){
+//     console.log('sha');
+// }else if(hk==7){
+//     console.log('ya');
+// }else{
+//     console.log('bunday hafta kuni yoq');
+// }
+    
+// let a = 7;
+// let b = 4;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+// let d = a > b;
+//  if(d){
+//     console.log(a);
+//  }else{
+//     console.log(b);
+//  }
+
+// let score=prompt('')
+// if(score>85){
+//     console.log('512000');
+// }else if(score>70){
+//     console.log('412000');
+// }else if(score>55){
+//     console.log('312000');
+// }
